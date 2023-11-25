fe9ac45d8f512c9349571d98fadd62f07c18879f
jinchandol <100199798+jinchandol@users.noreply.github.com>
2023-11-25 17:09:00 +0900

    Title: 세 수의 합, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 231125/세 수의 합/sum-of-three-num.py
+++ 231125/세 수의 합/sum-of-three-num.py
@@ -1,23 +1,48 @@
-n, k = map(int, input().split())
-num = list(map(int, input().split()))
+# n, k = map(int, input().split())
+# num = list(map(int, input().split()))
 
-count = dict()
+# count = dict()
 
-for elem in num:
-    if elem in count:
-        count[elem] += 1
+# for elem in num:
+#     if elem in count:
+#         count[elem] += 1
     
-    else:
-        count[elem] = 1
+#     else:
+#         count[elem] = 1
+
+# ans = 0
+# for i in range(n):
+#     count[num[i]] -= 1
+
+#     for j in range(i):
+#         diff = k - num[i] - num[j]
+
+#         if diff in count:
+#             ans += count[diff]
+
+# print(ans)
+
+n,k = map(int,input().split())
+
+num = list(map(int,input().split()))
+
+count= {}
 
 ans = 0
-for i in range(n):
-    count[num[i]] -= 1
 
-    for j in range(i):
-        diff = k - num[i] - num[j]
-
+for i in range(n): 
+    for j in range(i+1, n): 
+        elem1 = num[i]
+        elem2 = num[j]
+        diff = k - elem1 - elem2
+        
         if diff in count:
             ans += count[diff]
+        
+            
+    if num[i] in count:
+        count[num[i]] += 1 
+    else : 
+        count[num[i]] = 1 
 
 print(ans)
