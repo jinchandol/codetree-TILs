15a47736f9a1336584386edc1e06e0e9d72be68a
jinchandol <100199798+jinchandol@users.noreply.github.com>
2023-11-25 13:31:48 +0900

    Title: hashset 기본, Time: 417ms, Memory: 73MB, Status: Passed - CodeTree

--- 231125/hashset 기본/hashset-basic.py
+++ 231125/hashset 기본/hashset-basic.py
@@ -1,21 +1,41 @@
-n = int(input())
+# n = int(input())
 
+# s = set()
+
+# def do_it(p, q):
+#     global s
+#     if p == 'find':
+#         if q in s:
+#             print("true")
+#         else:
+#             print("false")
+
+#     elif p == 'add':
+#         s.add(q)
+    
+#     elif p == 'remove':
+#         s.remove(q)
+
+# for _ in range(n):
+#     action, num = input().split()
+#     do_it(action, num)
+
+
+# 변수 선언 및 입력:
+# 큰 차이는 없지만 아래와 같이 코드를 적용해도 괜찮음.
+n = int(input())
 s = set()
 
-def do_it(p, q):
-    global s
-    if p == 'find':
-        if q in s:
+for _ in range(n):
+    command, x = tuple(input().split())
+    x = int(x)
+
+    if command == "add":
+        s.add(x)
+    elif command == "remove":
+        s.remove(x)
+    else:
+        if x in s:
             print("true")
         else:
             print("false")
-
-    elif p == 'add':
-        s.add(q)
-    
-    elif p == 'remove':
-        s.remove(q)
-
-for _ in range(n):
-    action, num = input().split()
-    do_it(action, num)
