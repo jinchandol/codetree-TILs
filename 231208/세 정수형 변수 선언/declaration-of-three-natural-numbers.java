45da7d38fce7a65b5b1fe5ddb54f54562f91b5ea
jinchandol <100199798+jinchandol@users.noreply.github.com>
2023-12-08 23:36:10 +0900

    Title: 세 정수형 변수 선언, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231208/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int num1 = 7;
+        int num2 = 23;
+        int sum = num1+num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+    }
+}
