1c3eab282b2347de2ca99e87f39e3449bec60ab0
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-01-05 14:33:14 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240105/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -10,7 +10,8 @@ def recur(array, num, n):
         return
     if MAX_INT < array[num]:
         MAX_INT = array[num]
-        recur(array, num+1, n)
+    
+    recur(array, num+1, n)
     
 recur(num_arr, 0, n)
 
