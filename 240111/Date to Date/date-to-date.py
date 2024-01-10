9a1322f96d4a9191dbe7cd6eaf703e6aef151f90
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-01-11 00:52:19 +0900

    Title: Date to Date, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/Date to Date/date-to-date.py
+++ 240111/Date to Date/date-to-date.py
@@ -2,6 +2,6 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 m1, d1, m2, d2 = map(int, input().split())
 
-ans = (num_of_days[m1] - d1 + 1) + sum(num_of_days[m1+1:m2]) + d2
+ans = sum(num_of_days[m1:m2]) - d1 + d2 + 1
 
 print(ans)
