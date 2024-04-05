0fbe513215aa44152d89e5f20e85263308af747b
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-04-05 09:35:08 +0900

    Title: 외주 수익 최대화하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240405/외주 수익 최대화하기/max-of-outsourcing-profit.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, ans;
+    static int[][] task;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        task = new int[n+1][3];
+        ans = 0;
+        for (int i=1; i<=n; i++){
+            task[i][0] = i; // 일의 시점
+            task[i][1] = sc.nextInt() + i - 1; // 일의 종점
+            task[i][2] = sc.nextInt(); // 일의 수익
+        }
+
+        // recur(idx, profit);
+        recur(1, 0, 0);
+
+        System.out.println(ans);
+    }
+
+    private static void recur(int idx, int end, int profit){
+        if (idx == n+1){
+            ans = Math.max(ans, profit);
+            return;
+        }
+
+        if (end < task[idx][0]){
+            recur(idx + 1, task[idx][1], profit + task[idx][2]);
+            recur(idx + 1, end, profit);
+        } else {
+            recur(idx + 1, end, profit);
+        }
+    }
+}
