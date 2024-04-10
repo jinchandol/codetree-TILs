4584e95ee7189f5f8b0e9f4123476b4a275ab18d
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-04-11 02:05:58 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 1331ms, Memory: 26MB, Status: Passed - CodeTree

--- 240411/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240411/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,31 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int K, N;
-    static int[] arr;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        K = sc.nextInt();
-        N = sc.nextInt();
-        arr = new int[N];
-        
-        recur(0);
+public class Main {
+    public static int k, n;
+    public static ArrayList<Integer> selectedNums = new ArrayList<>();
+    
+    // 선택된 원소들을 출력해줍니다.
+    public static void printPermutation() {
+        for(int i = 0; i < selectedNums.size(); i++)
+            System.out.print(selectedNums.get(i) + " ");
+        System.out.println();
     }
 
-    private static void recur(int idx){
-        if (idx == N) {
-            for (int i=0; i<N; i++){
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+    public static void findPermutations(int cnt) {
+        // n개를 모두 뽑은 경우 답을 출력해줍니다.
+        if(cnt == n) {
+            printPermutation();
             return;
         }
 
-        for (int i=1; i<=K; i++){
-            arr[idx] = i;
-            recur(idx + 1);
+        // 1부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
+        for(int i = 1; i <= k; i++) {
+            selectedNums.add(i);
+            findPermutations(cnt + 1);
+            selectedNums.remove(selectedNums.size() - 1);
         }
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        k = sc.nextInt();
+        n = sc.nextInt();
+        
+		findPermutations(0);
+	}
 }
