5c34fd9b2c473b598d3c802c48e3ed89742e15e7
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-10-26 16:56:32 +0900

    Title: 수들의 합 최대화하기, Time: 239ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241026/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, ans;
+    public static ArrayList<Integer> nums = new ArrayList<>();
+    public static int[][] map;
+    public static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        map = new int[n][n];
+        visited = new boolean[n];
+
+        for (int r=0; r<n; r++) {
+            for (int c=0; c<n; c++) {
+                map[r][c] = sc.nextInt();
+            }
+        }
+
+        search(0);
+
+        System.out.println(ans);
+    }
+
+    public static int calc() {
+        int tmp = 0;
+        for (int idx = 0; idx < n; idx++) {
+            tmp += map[idx][nums.get(idx)];
+        }
+        return tmp;
+    }
+
+    public static void search(int row) {
+        if (row == n) {
+            ans = Math.max(ans, calc());
+            return;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            nums.add(i);
+
+            search(row + 1);
+
+            nums.remove(nums.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
