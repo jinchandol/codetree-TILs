0952db396723af6ef3d70b70f9b8c7fcc9aeabdd
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-10-26 16:25:09 +0900

    Title: 알파벳과 사칙연산, Time: 3055ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 241026/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 241026/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -5,43 +5,52 @@ public class Main {
     public static int ans = Integer.MIN_VALUE;
     public static int[] alphabet = new int[6];
     public static boolean[] visited = new boolean[6];
+    public static ArrayList<Integer> orders = new ArrayList<>();
     public static ArrayList<Integer> index = new ArrayList<>();
     public static ArrayList<Character> operation = new ArrayList<>();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] s = br.readLine().toCharArray();
+        int count = 0;
 
         for (char c : s) {
-            if (0 <= c - 'a' && c - 'a' <=5) index.add(c - 'a');
+            if (0 <= c - 'a' && c - 'a' <=5) {
+                orders.add(c - 'a');
+                if (visited[c - 'a']) continue;
+                else {
+                    index.add(c-'a');
+                    count++;
+                }
+            }
             else operation.add(c);
         }
 
-        if (index.size() == 1) {
+        if (orders.size() == 1) {
             ans = 4;
             System.out.println(ans);
             return;
         }
 
-        settingNumber(0);
+        settingNumber(0, count);
         System.out.println(ans);
     }
 
-    public static void settingNumber(int n) {
-        if (n == index.size()) {
+    public static void settingNumber(int n, int count) {
+        if (n == count) {
             performOperations();
             return;
         }
         for (int i = 1; i <= 4; i++) {
             alphabet[index.get(n)] = i;
-            settingNumber(n + 1);
+            settingNumber(n + 1, count);
         }
     }
 
     public static void performOperations() {
-        int result = alphabet[index.get(0)]; 
+        int result = alphabet[orders.get(0)]; 
         for (int i = 0; i < operation.size(); i++) {
-            int nextValue = alphabet[index.get(i + 1)];
+            int nextValue = alphabet[orders.get(i + 1)];
             result = doOperation(result, nextValue, operation.get(i));
         }
         ans = Math.max(ans, result);
