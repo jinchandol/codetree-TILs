2a04b4afaa58ad55a398cc8c2955883b0f5ddc2a
jinchandol <100199798+jinchandol@users.noreply.github.com>
2024-11-27 00:24:29 +0900

    Title: 가장 짧은 부분합, Time: 669ms, Memory: 33MB, Status: Wrong Answer - CodeTree

---
+++ 241127/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, s;
+    public static int[] arr;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        s = sc.nextInt();
+
+        int[] arr = new int[n+1];
+
+        for (int i=1; i<=n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int ans = Integer.MAX_VALUE;
+
+        int sumVal = 0;
+        int j = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            while(j + 1 <= n && sumVal + arr[j + 1] < s) {
+                sumVal += arr[j + 1];
+                j++;
+            }
+            
+            ans = Math.min(ans, j - i + 2);
+            
+            sumVal -= arr[i];
+        }
+
+        System.out.print(ans);
+    }
+}
