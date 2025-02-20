6ee9317bc55cb87b2283cf9f1c153e8c0fb7c6d4
jinchandol <100199798+jinchandol@users.noreply.github.com>
2025-02-20 13:30:55 +0900

    Title: 단어 출력, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

--- 250220/단어 출력/print-word.java
+++ 250220/단어 출력/print-word.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.println("World");
+        System.out.println("Hello");
     }
 }
